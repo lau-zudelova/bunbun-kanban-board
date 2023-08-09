84fa6c196bb3211987c4f9d6ba2c79409e2cf9a5
Laura Žúdelová <laura.zudelova@gmail.com>
2023-08-09 20:31:02 +0200

    implements todo preview

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,6 +4,8 @@ import Menu from "../Modals/Menu";
 import { TYPES } from "../Classes/Types";
 import { useGlobalContext } from "../GlobalContext";
 import CardDetail from "../Modals/CardDetail";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export default function Card({ card }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -37,10 +39,17 @@ export default function Card({ card }) {
     );
   };
 
+  function parseCheckboxes() {
+    const regex = /(- \[(?: |x)\] .*)(\n|$)/g;
+    const string = card.message;
+    const result = string.match(regex);
+    return result.join("");
+  }
+
   return (
     <div
       className={
-        `Card flex justify-between hover:cursor-pointer bg-gray-800 hover:text-violet-400 h-auto rounded-md p-2 mb-4 border-t border-t-blue-200 border-opacity-10 shadow-md items-center transition-transform ` +
+        `Card flex flex-col hover:cursor-pointer bg-gray-800 hover:text-violet-400 h-auto rounded-md p-2 mb-4 border-t border-t-blue-200 border-opacity-10 shadow-md transition-transform ` +
         card.color
       }
       draggable={!isEditable}
@@ -50,64 +59,91 @@ export default function Card({ card }) {
       }}
       data-cardid={card.id}
     >
-      {isEditable ? (
-        <input
-          autoFocus
-          type="text"
-          className="w-full p-2 rounded-md bg-gray-900 border border-gray-700 text-white font-semibold focus:outline-none focus:border-violet-400 caret-violet-400 selection:bg-violet-200 selection:text-violet-900"
-          value={titleInput}
-          onChange={(e) => setTitleInput(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" && e.shiftKey) {
-              saveTitle(e.target.value);
-              addCard(card.containerId);
-            } else if (e.key === "Enter") {
-              saveTitle(e.target.value);
-            } else if (e.key === "Escape") {
-              if (card.title === "") deleteCard(card);
-              else {
-                e.target.value = card.title;
-                setTitleInput(card.title);
-                setIsEditable(false);
+      <div className="flex justify-between items-center pointer-events-none">
+        {isEditable ? (
+          <input
+            autoFocus
+            type="text"
+            className="w-full p-2 rounded-md bg-gray-900 border border-gray-700 text-white font-semibold focus:outline-none focus:border-violet-400 caret-violet-400 selection:bg-violet-200 selection:text-violet-900"
+            value={titleInput}
+            onChange={(e) => setTitleInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && e.shiftKey) {
+                saveTitle(e.target.value);
+                addCard(card.containerId);
+              } else if (e.key === "Enter") {
+                saveTitle(e.target.value);
+              } else if (e.key === "Escape") {
+                if (card.title === "") deleteCard(card);
+                else {
+                  e.target.value = card.title;
+                  setTitleInput(card.title);
+                  setIsEditable(false);
+                }
               }
-            }
-          }}
+            }}
+          />
+        ) : (
+          <>
+            {card.title === "---" ? (
+              <hr className="w-full h-full border border-gray-500" />
+            ) : (
+              <p className="w-[90%] font-semibold break-words">{card.title}</p>
+            )}
+          </>
+        )}
+        <CardDetail
+          isOpen={isDetailOpen}
+          close={() => setIsDetailOpen(false)}
+          card={card}
         />
-      ) : (
+        <button
+          id={card.id}
+          className="p-1 justify-self-end text-white hover:bg-gray-700 rounded-md hover:text-violet-400 transition-all duration-100 pointer-events-auto"
+          onClick={(event) => {
+            event.stopPropagation();
+            setIsMenuOpen(true);
+            getCoords();
+            parseCheckboxes();
+          }}
+        >
+          <DotsThreeVertical size={18} />
+        </button>
+        <Menu
+          isOpen={isMenuOpen}
+          close={() => setIsMenuOpen(false)}
+          coords={coords}
+          type={TYPES.CARD}
+          object={card}
+          setIsEditable={setIsEditable}
+        />
+      </div>
+      {card.message ? (
         <>
-          {card.title === "---" ? (
-            <hr className="w-full h-full border border-gray-500 pointer-events-none" />
-          ) : (
-            <p className="w-[90%] pointer-events-none font-semibold break-words">
-              {card.title}
-            </p>
-          )}
+          <hr className="border border-gray-700 my-2" />
+          <ReactMarkdown
+            children={parseCheckboxes()}
+            remarkPlugins={[remarkGfm]}
+            components={{
+              input: ({ node, ...props }) => {
+                return (
+                  <input
+                    type="checkbox"
+                    onChange={(e) => {
+                      const siblingContent =
+                        (props.checked ? "- [x]" : "- [ ]") +
+                        e.target.parentElement.textContent;
+                      toggleInputCheckbox(siblingContent, props.checked);
+                    }}
+                    checked={props.checked ? true : false}
+                  />
+                );
+              },
+            }}
+            className="h-full w-full p-2 pt-0 text-white pointer-events-none"
+          />
         </>
-      )}
-      <CardDetail
-        isOpen={isDetailOpen}
-        close={() => setIsDetailOpen(false)}
-        card={card}
-      />
-      <button
-        id={card.id}
-        className="p-1 justify-self-end text-white hover:bg-gray-700 rounded-md hover:text-violet-400 transition-all duration-100"
-        onClick={(event) => {
-          event.stopPropagation();
-          setIsMenuOpen(true);
-          getCoords();
-        }}
-      >
-        <DotsThreeVertical size={18} />
-      </button>
-      <Menu
-        isOpen={isMenuOpen}
-        close={() => setIsMenuOpen(false)}
-        coords={coords}
-        type={TYPES.CARD}
-        object={card}
-        setIsEditable={setIsEditable}
-      />
+      ) : null}
     </div>
   );
 }
